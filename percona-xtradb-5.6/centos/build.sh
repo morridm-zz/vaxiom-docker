9e3ddf469b96c98517e979c0588a0d120d6336af
David Morris <david.morris@axpm.com>
2014-05-02 19:17:49 -0500

    no message

--- percona-xtradb-5.6/centos/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [[ ! -f id_rsa.pub ]]; then
-    echo "No public ssh key found. Generating a new ssh key"
-    echo ""
-    ssh-keygen -t rsa -N "" -f id_rsa
-fi
-docker build -rm=true -t $USER/centos:pxc56 .
