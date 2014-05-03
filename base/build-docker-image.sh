5ecd07f65af48f1da1256896f1203929c3182e65
David Morris <david.morris@axpm.com>
2014-05-03 17:02:07 -0500

    no message

--- base/build-docker-image.sh
+++ base/build-docker-image.sh
@@ -72,25 +72,25 @@ deleteDockerImage() {
 
         echo "INFO: Searching for $IMAGE_NAME image(s)..."
         echo "INFO: DELETING EXISTING DOCKER CONTAINERS FOR $IMAGE_NAME..."
-        for container in `docker ps -a | grep $IMAGE_NAME`; do
+        for container in `sudo docker ps -a | grep $IMAGE_NAME`; do
                 DOCKER_IMAGES_STR=$(echo '$container' | sed 's/  */\ /g')
                 declare -a DOCKER_IMAGE_ARRAY=($DOCKER_IMAGES_STR)
                 DOCKER_CONTAINER_ID${DOCKER_IMAGE_ARRAY[0]}
                 echo "INFO: Deleting container: $DOCKER_CONTAINER_ID ..."
-                docker rm -f $DOCKER_CONTAINER_ID
+                sudo docker rm -f $DOCKER_CONTAINER_ID
                 if [ ! $? -eq 0 ];then
                         echo "WARNING: Error deleting container $container"
                 fi
         done
 
         echo "INFO: DELETING EXISTING DOCKER IMAGES..."
-        for image in `docker images -a | grep $IMAGE_NAME`; do
+        for image in `sudo docker images -a | grep $IMAGE_NAME`; do
                 local DOCKER_IMAGES_STR=$(echo '$image' | sed 's/  */\ /g')
                 declare -a DOCKER_IMAGE_ARRAY=($DOCKER_IMAGES_STR)
                 DOCKER_IMAGE_ID=${DOCKER_IMAGE_ARRAY[2]}
 
                 echo "INFO: Deleting container: $image ..."
-                docker rmi -f $DOCKER_IMAGE_ID
+                sudo docker rmi -f $DOCKER_IMAGE_ID
                 if [ $? -eq 0 ];then
                         echo "WARNING: Error deleting image $image"
                 fi
